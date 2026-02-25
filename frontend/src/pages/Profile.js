7f2700cf5d0e8b736557c974850997e866d6be16
Mvivek123 <2400033104@kluniversity.in>
2026-02-25 14:54:28 +0530

    Fix unused import in Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useClubs } from '../context/ClubsContext';
 import { useEvents } from '../context/EventsContext';
